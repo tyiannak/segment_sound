dafc42fefa6102b331131e45bc062f041f8ed77a
Theodoros Giannakopoulos <tyiannak@gmail.com>
2021-10-09 10:54:18 +0300

    smooth and threshold added as cl arguments

--- segment_audio.py
+++ segment_audio.py
@@ -5,14 +5,14 @@ from pyAudioAnalysis import audioSegmentation as aS
 
 def parse_arguments():
     segment = argparse.ArgumentParser(description="Audio segment")
-#    record_analyze.add_argument("-w", "--win_size",
-#                                  type=float, choices=[0.25, 0.5, 0.75, 1, 2],
-#                                  default=1, help="Recording block size")
-#    record_analyze.add_argument("-fs", "--samplingrate", type=int,
-#                                  choices=[4000, 8000, 16000, 32000, 44100],
-#                                  default=8000, help="Recording block size")
-#    record_analyze.add_argument("--chromagram", action="store_true",
-#                                  help="Show chromagram")
+    segment.add_argument("-s", "--smooth", type=float, default=0.1,
+                         help="Probability smoothing window: the higher"
+                              "this param is, the more difficult is to "
+                              "detect small changes in the signal. "),
+    segment.add_argument("-t", "--threshold", default=0.5, type=float,
+                         help="Thresholding weight. The higher this value is"
+                              "the fewer the segments that survive in the "
+                              "final segmentation")
     segment.add_argument("-i", "--input", help="Input WAV file")
     return segment.parse_args()
 
@@ -20,8 +20,10 @@ def parse_arguments():
 if __name__ == "__main__":
     args = parse_arguments()
     input = args.input
+    t = args.threshold
+    s = args.smooth
     fs, x = aIO.read_audio_file(input)
-    segs = aS.silence_removal(x, fs, 0.05, 0.01, smooth_window=0.1,
-                              weight=0.02, plot=True)
+    segs = aS.silence_removal(x, fs, 0.04, 0.02, smooth_window=s,
+                              weight=t, plot=True)
     for s in segs:
         print(s)
